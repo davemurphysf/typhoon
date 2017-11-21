9a850614ec4f42dc4abf1b869d8cf5097986fbfd
David Murphy <dave.w.murphy@gmail.com>
2017-11-21 15:32:24 -0800

    Add load_balancer and floating_ip output

--- digital-ocean/container-linux/kubernetes/outputs.tf
+++ digital-ocean/container-linux/kubernetes/outputs.tf
@@ -21,3 +21,15 @@ output "workers_ipv4" {
 output "workers_ipv6" {
   value = ["${digitalocean_droplet.workers.*.ipv6_address}"]
 }
+
+output "load_balancers_ipv4" {
+  value = ["${digitalocean_droplet.load_balancers.*.ipv4_address}"]
+}
+
+output "load_balancers_ipv6" {
+  value = ["${digitalocean_droplet.load_balancers.*.ipv6_address}"]
+}
+
+output "floating_ip" {
+  value = "${digitalocean_floating_ip.load_balancers.ip_address}"
+}
