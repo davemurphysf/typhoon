0c1403eda6024a22fa9df3298524588b9dd4d21c
David Murphy <dave.w.murphy@gmail.com>
2017-11-26 07:38:58 -0800

    Break out workers/controllers and allow 443 access for kubectl (for now)

--- digital-ocean/container-linux/kubernetes/network.tf
+++ digital-ocean/container-linux/kubernetes/network.tf
@@ -1,18 +1,62 @@
-resource "digitalocean_firewall" "rules" {
-  name = "${var.cluster_name}"
+resource "digitalocean_firewall" "workers" {
+  name = "${var.cluster_name}-workers"
 
-  tags = ["${var.cluster_name}-controller", "${var.cluster_name}-worker"]
+  tags = ["${var.cluster_name}-worker"]
 
-  # allow ssh, http/https ingress, and peer-to-peer traffic
+  depends_on = ["digitalocean_droplet.workers"]
+
   inbound_rule = [
     {
       protocol         = "tcp"
       port_range       = "22"
       source_addresses = ["0.0.0.0/0", "::/0"]
     },
+     {
+      protocol         = "tcp"
+      port_range       = "443"
+      source_addresses = ["0.0.0.0/0", "::/0"]
+    },
+    {
+      protocol    = "udp"
+      port_range  = "all"
+      source_tags = ["${digitalocean_tag.controllers.name}", "${digitalocean_tag.workers.name}", "${digitalocean_tag.load_balancer.name}"]
+    },
+    {
+      protocol    = "tcp"
+      port_range  = "all"
+      source_tags = ["${digitalocean_tag.controllers.name}", "${digitalocean_tag.workers.name}", "${digitalocean_tag.load_balancer.name}"]
+    },
+  ]
+
+  outbound_rule = [
+    {
+      protocol              = "icmp"
+      destination_addresses = ["0.0.0.0/0", "::/0"]
+    },
+    {
+      protocol              = "udp"
+      port_range            = "all"
+      destination_addresses = ["0.0.0.0/0", "::/0"]
+    },
+    {
+      protocol              = "tcp"
+      port_range            = "all"
+      destination_addresses = ["0.0.0.0/0", "::/0"]
+    },
+  ]
+}
+
+resource "digitalocean_firewall" "controller" {
+  name = "${var.cluster_name}-controllers"
+
+  tags = ["${var.cluster_name}-controller"]
+
+  depends_on = ["digitalocean_droplet.controllers"]
+
+  inbound_rule = [
     {
       protocol         = "tcp"
-      port_range       = "80"
+      port_range       = "22"
       source_addresses = ["0.0.0.0/0", "::/0"]
     },
     {
@@ -23,12 +67,12 @@ resource "digitalocean_firewall" "rules" {
     {
       protocol    = "udp"
       port_range  = "all"
-      source_tags = ["${digitalocean_tag.controllers.name}", "${digitalocean_tag.workers.name}"]
+      source_tags = ["${digitalocean_tag.controllers.name}", "${digitalocean_tag.workers.name}", "${digitalocean_tag.load_balancer.name}"]
     },
     {
       protocol    = "tcp"
       port_range  = "all"
-      source_tags = ["${digitalocean_tag.controllers.name}", "${digitalocean_tag.workers.name}"]
+      source_tags = ["${digitalocean_tag.controllers.name}", "${digitalocean_tag.workers.name}", "${digitalocean_tag.load_balancer.name}"]
     },
   ]
 
