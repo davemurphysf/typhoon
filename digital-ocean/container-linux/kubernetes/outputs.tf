8e0fb6dd91dd34e30ce2e0356c95d1425b0b3406
David Murphy <dave.w.murphy@gmail.com>
2017-12-11 10:41:23 -0800

    Breakout lb and db into seperate modules

--- digital-ocean/container-linux/kubernetes/outputs.tf
+++ digital-ocean/container-linux/kubernetes/outputs.tf
@@ -25,23 +25,3 @@ output "workers_ipv4_private" {
 output "workers_ipv6" {
   value = ["${digitalocean_droplet.workers.*.ipv6_address}"]
 }
-
-output "load_balancers_ipv4" {
-  value = ["${digitalocean_droplet.load_balancers.*.ipv4_address}"]
-}
-
-output "load_balancers_ipv6" {
-  value = ["${digitalocean_droplet.load_balancers.*.ipv6_address}"]
-}
-
-output "floating_ip" {
-  value = "${digitalocean_floating_ip.load_balancers.ip_address}"
-}
-
-output "asset_dir" {
-  value = "${var.asset_dir}"
-}
-
-output "cluster_name" {
-  value = "${var.cluster_name}"
-}
